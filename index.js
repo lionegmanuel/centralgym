9ebdce82f2a4bde4156861b10fa072e19dbe414e
MagnuZvn <manuel.lg077@gmail.com>
2023-08-26 19:09:46 -0300

    funcionalidades js => indexedDB con consultas enviadas desde el formulario

--- index.js
+++ index.js
@@ -1,8 +1,65 @@
-//horarios del gimnasio
+//base de datos (personas registradas en el gimnasio)
+const request = indexedDB.open('consults')
+request.addEventListener('upgradeneeded', () => {
+  const db = request.result
+  const dbName = db.name
+  const objectStore = db.createObjectStore('consult', {
+    autoIncrement: true
+  })
+  console.log('Base de datos: ', dbName)
+})
+
+const getDataDB = (idbTransactionMode) => { //recibe como parámetro el modo (escritura o lectura)
+  //retorna => 
+  //1-base de datos - Nombre de la base de datos - 3 - IDBTranstacion - 4 - ObjectStore
+  const db = request.result
+  const dbName = db.name
+  const transtacion = db.transaction('consult', idbTransactionMode)
+  const objectStore = transtacion.objectStore('consult')
+  return [db, dbName, transtacion, objectStore]
+}
+//CRUD
+const addObject = object => {
+  const dbData = getDataDB('readwrite') //dataArray
+  const objectStore = dbData[3]
+  const addRequest = objectStore.add(object)
+
+  addRequest.addEventListener('success', () => console.log('Objeto agregado a DB'))
+  addRequest.addEventListener('error', () => console.log('El objeto NO pudo ser agregado a DB'))
+}
+
+const readObject = () => {
+  const result = []
+  const dbData = getDataDB('readonly')
+  const readPointer = dbData[3].openCursor()
+  readPointer.addEventListener('success', () => {
+    if (readPointer.result != null) {
+      console.log(readPointer.result.value)
+      result.push(readPointer.result.value)
+      readPointer.result.continue()
+    }
+  })
+  return result
+}
+
+const updateObject = (object, key) => {
+  const dbData = getDataDB('readwrite')
+  const updateRequest = dbData[3].put(object, key)
+  updateRequest.addEventListener('success', () => console.log(`Objeto con key ${key} fue modificado con éxito`))
+  updateRequest.addEventListener('error', () => console.log(`El objeto con key de referencia ${key} NO pudo ser modificado`))
+}
+
+const deleteObject = key => {
+  const dbData = getDataDB('readwrite')
+  const deleteRequest = dbData[3].delete(key)
+  deleteRequest.addEventListener('success', () => console.log(`Objeto con key de referencia ${key} fue eliminado con éxito`))
+  deleteRequest.addEventListener('error', () => console.log(`El objeto con key de referencia ${key} NO pudo ser eliminado`))
+}
+
 const muscleHours = document.querySelectorAll('.muscle')
 const funcionalHours = document.querySelectorAll('.funcional')
 const aboutGymContainer = document.getElementById('aboutGym')
-const descriptionAboutGym = aboutGymContainer.firstElementChild
+const descriptionAboutGym = aboutGymContainer ? aboutGymContainer.querySelector('.description') : null
 
 const addHeaderHour = () => {
   //encabezados
@@ -37,12 +94,13 @@ if (window.matchMedia("(max-width: 890px)").matches) {
 }
 
 
-//envio del formulario
+//envio del formulario => validaciones
 const contactForm = document.getElementById('form')
 const sendButtonContactForm = document.querySelector('.sendButton')
 sendButtonContactForm.addEventListener('click', (e) => {
   e.preventDefault();
   //proceso de validaciones
+  let showAlert = false; //permite detener la ejecucion del flujo "switch" cuando ya se muestre un alert
   const name = document.getElementById('name').value
   const lastName = document.getElementById('lastName').value
   const email = document.getElementById('email').value
@@ -50,6 +108,15 @@ sendButtonContactForm.addEventListener('click', (e) => {
 
   if (/^[a-zA-Z\s]{4,}$/.test(name) && /^[a-zA-Z\s]{4,}$/.test(lastName) && /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email) && /^[a-zA-Z0-9.!$\s]{40,}/.test(message)
   ) {
+    const consult = {
+      nombre: name,
+      apellido: lastName,
+      email: email,
+      mensaje: message
+    }
+    //guarda el objeto dentro de DB
+    addObject(consult)
+    console.log('Consulta registrada en DB')
     alert('¡Consulta enviada con éxito!')
     document.getElementById('name').value = ''
     document.getElementById('lastName').value = ''
@@ -60,33 +127,48 @@ sendButtonContactForm.addEventListener('click', (e) => {
     switch (name) {
       case '':
         alert('El campo nombre está vacío')
+        showAlert = true;
         break
       default:
         if (!(/^[a-zA-Z\s]{4,}$/.test(name))) {
           alert('Ingrese un nombre válido')
+          showAlert = true;
         }
     }
-    switch (lastName) { //en el apellido se permite que esté vacío
-      case '':
-        alert('El campo apellido está vacío')
-        break
-      default:
-        if (!(/^[a-zA-Z\s]{4,}$/.test(lastName))) {
-          alert('Ingrese un apellid válido')
-        }
+    if (!showAlert) {
+      switch (lastName) { //en el apellido se permite que esté vacío
+        case '':
+          alert('El campo apellido está vacío')
+          showAlert = true;
+          break
+        default:
+          if (!(/^[a-zA-Z\s]{4,}$/.test(lastName))) {
+            alert('Ingrese un apellido válido')
+            showAlert = true;
+          }
+      }
     }
-    switch (email) {
-      case '':
-        alert('El campo de email está vacío')
-        break
-      default:
-        if (!(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email))) alert('Email inválido. Por favor, ingrese un email válido')
+    if (!showAlert) {
+      switch (email) {
+        case '':
+          alert('El campo de email está vacío')
+          showAlert = true;
+          break
+        default:
+          if (!(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email))) {
+            alert('Email inválido. Por favor, ingrese un email válido')
+            showAlert = true;
+          }
+      }
     }
-    switch (message) {
-      default:
-        if (!(/^[a-zA-Z0-9.!$\s]{40,}/.test(message))) alert('Ingrese una consulta válida')
+    if (!showAlert) {
+      switch (message) {
+        default:
+          if (!(/^[a-zA-Z0-9.!$\s]{40,}/.test(message))) {
+            alert('Ingrese una consulta válida')
+            showAlert = true;
+          }
+      }
     }
-
   }
 })
-
